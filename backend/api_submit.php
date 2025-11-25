86f9985db525e640e57e27158d6e082b933c170c
André Ventura <ti@asventura.com.br>
2025-11-25 18:39:28 -0300

    feat: aprimorar configuração de CORS para permitir origens dinâmicas

--- backend/api_submit.php
+++ backend/api_submit.php
@@ -2,9 +2,19 @@
 require 'config.php';
 
 // 1. Configuração CORS/JSON
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json; charset=UTF-8");
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    exit(0);
+}
 
 // 2. Recebe os dados
 $input = json_decode(file_get_contents("php://input"), true);
